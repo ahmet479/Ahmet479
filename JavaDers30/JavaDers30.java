135c199500c2d7d12c1962da6ae4551f846f5005
ahmet479 <115145728+ahmet479@users.noreply.github.com>
2022-11-21 08:02:32 +0100

    Add files via upload

---
+++ JavaDers30/JavaDers30.java
@@ -0,0 +1,33 @@
+package JavaDers30;
+
+public class JavaDers30 {
+// Array
+// 1.int[] sayilar = new int [5];
+// 2.int[]sayilar = {10,20,50,40,15}
+//Dikkat edilmesi gerekenler
+public static void main(String[]args) {
+	String[]isimler = new String[3];
+/*
+	isimler[0] = "Ayse";
+	isimler[1] = "Hilal";
+	isimler[2] = "Esad";
+	
+	System.out.println(isimler[0]);
+	System.out.println(isimler[1]);
+	System.out.println(isimler[2]);
+	System.out.println(isimler[3]);
+*/
+	isimler[0] = "Ayse";
+	isimler[1] = "Hilal";
+	isimler[2] = "Esad";
+	isimler[2] = "Esma";
+	
+	System.out.println(isimler[0]);
+	System.out.println(isimler[1]);
+		
+	System.out.println(isimler[3]);
+	
+
+
+}
+}
